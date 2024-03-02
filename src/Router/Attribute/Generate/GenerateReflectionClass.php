02f55ca0f1c62c9f41d65c993b2dd8ceb0da08c7
bxel07 <yogipratama.ut@gmail.com>
2024-03-02 18:29:47 +0700

    fixing router generator

--- src/Router/Attribute/Generate/GenerateReflectionClass.php
+++ src/Router/Attribute/Generate/GenerateReflectionClass.php
@@ -2,6 +2,7 @@
 
 namespace Xel\Async\Router\Attribute\Generate;
 
+use InvalidArgumentException;
 use ReflectionAttribute;
 use ReflectionClass;
 use ReflectionException;
@@ -10,27 +11,19 @@ use Xel\Async\Router\Attribute\Middlewares;
 use Xel\Async\Router\Attribute\Router;
 
 const CACHE_NAME = "class.cache";
-
-
 /**
  * @param class-string[] $loader
  * @param string $cachePath
- * @return void
+ * @param string $build
+ * @return array|null
  */
-function loaderClass(array $loader, string $cachePath): void
+function loaderClass(array $loader, string $cachePath, string $build = 'dev'): ?array
 {
-    try {
-        if (!file_exists($cachePath."/".CACHE_NAME)){
-            $class = extractClass($loader);
-
-            // ? CacheAble
-            generateCacheClass($class, $cachePath);
-        }
-    } catch (RuntimeException $exception) {
-        // Print a message (you might want to log it instead in a real-world scenario)
-        echo 'Exception in loaderClass: ' . $exception->getMessage() . PHP_EOL;
-        // Rethrow the exception
+    if ($build !== "dev"){
+        return extractClass($loader);
     }
+
+    return loadCachedClass($cachePath);
 }
 
 /**
@@ -78,14 +71,17 @@ function extractClass(array $loader): array
  * Cache Class Generator
  * @param array<int|string, mixed> $class
  * @param string $path
+ * @return void
  */
 function generateCacheClass(array $class, string $path): void
 {
     try {
         file_put_contents($path . "/" . CACHE_NAME, '<?php return ' . var_export($class, true) . ';');
     } catch (RuntimeException $exception) {
-        // Print a message (you might want to log it instead in a real-world scenario)
-        echo 'Exception in generateCacheClass: ' . $exception->getMessage() . PHP_EOL;
+        // Log the exception (you might want to use a proper logging mechanism)
+        error_log('Exception in generateCacheClass: ' . $exception->getMessage());
+        // Re-throw the exception to be caught by higher-level error handling
+        throw $exception;
     }
 }
 
@@ -93,33 +89,13 @@ function generateCacheClass(array $class, string $path): void
  * @param string $cachePath
  * @return array<int|string, mixed>|null
  */
-function loadCachedClass(string $cachePath): array|null
+function loadCachedClass(string $cachePath): ?array
 {
-    try {
-        if (file_exists($cachePath . "/" . CACHE_NAME)) {
-            return require $cachePath . "/" . CACHE_NAME;
-        }
-
-    } catch (RuntimeException $exception) {
-        echo 'Exception: ' . $exception->getMessage() . PHP_EOL;
+    if (!file_exists($cachePath . "/" . CACHE_NAME)) {
+        throw new InvalidArgumentException("File not provided: $cachePath".'/'.CACHE_NAME);
     }
-    return null;
-}
+    return require $cachePath . "/" . CACHE_NAME;
 
-/**
- * @param class-string[] $loader
- * @param string $cachePath
- * @return void
- */
-function renewClass(array $loader, string $cachePath): void
-{
-    try {
-        $class = extractClass($loader);
-        generateCacheClass($class, $cachePath);
-    } catch (RuntimeException $exception) {
-        // Print a message (you might want to log it instead in a real-world scenario)
-        echo 'Exception in renewClass: ' . $exception->getMessage() . PHP_EOL;
-    }
 }
 
 /**
